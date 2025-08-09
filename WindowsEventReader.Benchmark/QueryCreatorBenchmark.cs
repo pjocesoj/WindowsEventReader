3700c509d7f195b6f3cb95371de1a748102dcb77
martin novak <martinnovakmelnik@live.com>
2025-08-10 00:35:15 +0200

    benchmark

---
+++ WindowsEventReader.Benchmark/QueryCreatorBenchmark.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace WindowsEventReader.Benchmark
+{
+    [RankColumn]
+    public class QueryCreatorBenchmark
+    {
+        [Benchmark]
+        public void StartDateQueryBenchmark()
+        {
+            DateTime startDate = new DateTime(2023, 10, 1, 12, 0, 0);
+            string query = QueryCreator.StartDateQuery(startDate);
+        }
+        [Benchmark]
+        public void EventIdQueryBenchmark()
+        {
+            string eventId = "1000";
+            string query = QueryCreator.EventIdQuery(eventId);
+        }
+        [Benchmark]
+        public void EventIdsQueryBenchmark()
+        {
+            IEnumerable<string> eventIds = new List<string> { "1000", "2000", "3000" };
+            string query = QueryCreator.EventIdQuery(eventIds);
+        }
+    }
+}
