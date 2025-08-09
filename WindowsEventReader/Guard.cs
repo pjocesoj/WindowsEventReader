f75c6b7b375592351b6235639ec2a44de443251c
martin novak <martinnovakmelnik@live.com>
2025-08-10 00:03:02 +0200

    guards

---
+++ WindowsEventReader/Guard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsEventReader
+{
+    public static class Guard
+    {
+        public static void NotNullOrEmpty(string value, string parameterName)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Value cannot be null or empty.", parameterName);
+            }
+        }
+        public static void NotNullOrEmpty(IEnumerable<string> values, string parameterName)
+        {
+            if (values == null || !values.Any())
+            {
+                throw new ArgumentException("Collection cannot be null or empty.", parameterName);
+            }
+        }
+        public static void IsNumber(string value, string parameterName)
+        {
+            if (!int.TryParse(value, out _))
+            {
+                throw new ArgumentException("Value must be a valid number.", parameterName);
+            }
+        }
+    }
+}
--- WindowsEventReader/QueryCreator.cs
+++ WindowsEventReader/QueryCreator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace WindowsEventReader
@@ -38,6 +39,8 @@ namespace WindowsEventReader
         /// <param name="star">* on beginning (whole query) or not (on end of another)</param>
         public static string EventIdQuery(string eventId, bool star = true)
         {
+            Guard.NotNullOrEmpty(eventId, nameof(eventId));
+
             if (star)
             {
                 return $"*[System[EventID={eventId}]]";
@@ -50,6 +53,8 @@ namespace WindowsEventReader
         /// <param name="eventId">event ID</param>
         public static string EventIdExpression(string eventId)
         {
+            Guard.NotNullOrEmpty(eventId, nameof(eventId));
+
             return $"EventID={eventId}";
         }
 
@@ -61,6 +66,8 @@ namespace WindowsEventReader
         /// <returns></returns>
         public static string EventIdQuery(IEnumerable<string> eventIds, bool star = true)
         {
+            Guard.NotNullOrEmpty(eventIds, nameof(eventIds));
+
             var sb = new StringBuilder(star ? "*[System[" : "[System[");
             foreach (var id in eventIds)
             {
@@ -76,6 +83,8 @@ namespace WindowsEventReader
         /// <param name="eventIds">collection of ids</param>
         public static string EventIdExpression(IEnumerable<string> eventIds)
         {
+            Guard.NotNullOrEmpty(eventIds, nameof(eventIds));
+
             var sb = new StringBuilder();
             foreach (var id in eventIds)
             {
@@ -95,6 +104,10 @@ namespace WindowsEventReader
         /// <param name="star">* on beginning (whole query) or not (on end of another)</param>
         public static string EventProviderAndIdQuery(string eventId, string providerFullName, bool star = true)
         {
+            Guard.NotNullOrEmpty(eventId, nameof(eventId));
+            Guard.NotNullOrEmpty(providerFullName, nameof(providerFullName));
+            Guard.IsNumber(eventId, nameof(eventId));
+
             if (star)
             {
                 return $"*[System[Provider[@Name='{providerFullName}'] and (EventID={eventId})]]";
@@ -109,6 +122,10 @@ namespace WindowsEventReader
         /// <returns></returns>
         public static string EventProviderAndIdExpression(string eventId, string providerFullName)
         {
+            Guard.NotNullOrEmpty(eventId, nameof(eventId));
+            Guard.NotNullOrEmpty(providerFullName, nameof(providerFullName));
+            Guard.IsNumber(eventId, nameof(eventId));
+
             return $"(Provider[@Name='{providerFullName}'] and (EventID={eventId}))";
         }
         #endregion
