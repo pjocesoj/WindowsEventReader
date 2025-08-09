ba80024465f69d7c8279399d37b81734b94f1182
martin novak <martinnovakmelnik@live.com>
2025-08-09 19:46:16 +0200

    Add project files.

---
+++ WindowsEventReader/EventLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+
+namespace ReadWindowsEvent
+{
+    public class EventLoader
+    {
+        public List<EventRecord> LoadEvents(string query)
+        {
+            List<EventRecord> events = new List<EventRecord>();
+
+            EventLogQuery eventLogQuery = new EventLogQuery("System", PathType.LogName, query);
+            using (EventLogReader eventLogReader = new EventLogReader(eventLogQuery))
+            {
+                try
+                {
+                    EventRecord eventRecord;
+                    while ((eventRecord = eventLogReader.ReadEvent()) != null)
+                    {
+                        events.Add(eventRecord);
+                    }
+                }
+                catch (EventLogException ex)
+                {
+                    throw;
+                }
+            }
+            return events;
+        }
+    }
+}
---
+++ WindowsEventReader/QueryCreator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadWindowsEvent
+{
+    public static class QueryCreator
+    {
+        #region Startdate
+        /// <summary>
+        /// create query string for filter events from date
+        /// </summary>
+        /// <param name="start">start date</param>
+        /// <param name="star">* on beginning (whole query) or not (on end of another)</param>
+        public static string StartDateQuery(DateTime start, bool star = true)
+        {
+            if (star)
+            {
+                return $"*[System[TimeCreated[@SystemTime >= '{start:yyyy-MM-ddTHH:mm:ss.fffZ}']]]";
+            }
+            return $"[System[TimeCreated[@SystemTime >= '{start:yyyy-MM-ddTHH:mm:ss.fffZ}']]]";
+        }
+        /// <summary>
+        /// create expression for filter events from date which can be part of complex query string 
+        /// </summary>
+        /// <param name="start">start date</param>
+        public static string StartDateExpression(DateTime start)
+        {
+            return $"TimeCreated[@SystemTime >= '{start:yyyy-MM-ddTHH:mm:ss.fffZ}']";
+        }
+        #endregion
+
+        #region EventID
+        /// <summary>
+        /// create query string for filter events by EventID
+        /// </summary>
+        /// <param name="eventId">event ID</param>
+        /// <param name="star">* on beginning (whole query) or not (on end of another)</param>
+        public static string EventIdQuery(string eventId, bool star = true)
+        {
+            if (star)
+            {
+                return $"*[System[EventID={eventId}]]";
+            }
+            return $"[System[EventID={eventId}]]";
+        }
+        /// <summary>
+        /// create expression for filter events by EventID which can be part of complex query string
+        /// </summary>
+        /// <param name="eventId">event ID</param>
+        public static string EventIdExpression(string eventId)
+        {
+            return $"EventID={eventId}";
+        }
+
+        /// <summary>
+        /// create query string for filter events by multiple EventIDs
+        /// </summary>
+        /// <param name="eventIds">collection of ids</param>
+        /// <param name="star">* on beginning (whole query) or not (on end of another)</param>
+        /// <returns></returns>
+        public static string EventIdQuery(IEnumerable<string> eventIds, bool star = true)
+        {
+            var sb = new StringBuilder(star ? "*[System[" : "[System[");
+            foreach (var id in eventIds)
+            {
+                sb.Append($"EventID={id} or ");
+            }
+            sb.Remove(sb.Length - 4, 4); // Remove the last " or "
+            sb.Append("]]");
+            return sb.ToString();
+        }
+        /// <summary>
+        /// create expression for filter events by multiple EventIDs which can be part of complex query string
+        /// </summary>
+        /// <param name="eventIds">collection of ids</param>
+        public static string EventIdExpression(IEnumerable<string> eventIds)
+        {
+            var sb = new StringBuilder();
+            foreach (var id in eventIds)
+            {
+                sb.Append($"EventID={id} or ");
+            }
+            sb.Remove(sb.Length - 4, 4); // Remove the last " or "
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Provider and EventID
+        /// <summary>
+        /// create query string for filter events by EventID and Provider Name
+        /// </summary>
+        /// <param name="eventId">event ID</param>
+        /// <param name="providerFullName">provider full name (including Microsoft-Windows and other prefixes)</param>
+        /// <param name="star">* on beginning (whole query) or not (on end of another)</param>
+        public static string EventProviderAndIdQuery(string eventId, string providerFullName, bool star = true)
+        {
+            if (star)
+            {
+                return $"*[System[Provider[@Name='{providerFullName}'] and (EventID={eventId})]]";
+            }
+            return $"[System[Provider[@Name='{providerFullName}'] and (EventID={eventId})]]";
+        }
+        /// <summary>
+        /// create expression for filter events by EventID and Provider Name which can be part of complex query string
+        /// </summary>
+        /// <param name="eventId">event ID</param>
+        /// <param name="providerFullName">provider full name (including Microsoft-Windows and other prefixes)</param>
+        /// <returns></returns>
+        public static string EventProviderAndIdExpression(string eventId, string providerFullName)
+        {
+            return $"(Provider[@Name='{providerFullName}'] and (EventID={eventId}))";
+        }
+        #endregion
+    }
+}
